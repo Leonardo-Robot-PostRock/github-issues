c23c0e641a4556c023dd413086c9ca7d18e31bb6
Leonardo <leonardo38361@gmail.com>
2026-01-07 11:29:41 -0300

    feat: improve useIssue hook with comments query

--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -1,15 +1,31 @@
 import { useQuery } from "@tanstack/react-query"
-import { getIssue } from "../actions"
+import { getIssue, getIssueComments } from "../actions"
 
 export const useIssue = (issueNumber: number) => {
 
     const issueQuery = useQuery({
-        queryKey: ['issue', issueNumber],
+        queryKey: ['issues', issueNumber],
         queryFn: () => getIssue(issueNumber),
         staleTime: 1000 * 60,
     })
 
+    // const commentsQuery = useQuery({
+    //     queryKey: ['issues', issueNumber, 'comments'],
+    //     queryFn: () => getIssueComments(issueNumber),
+    //     staleTime: 1000 * 60,
+    // })
+
+    const issueNumberData = issueQuery.data?.number;
+
+    const commentsQuery = useQuery({
+        queryKey: ['issues', issueNumberData, 'comments'],
+        queryFn: () => getIssueComments(issueNumberData!),
+        staleTime: 1000 * 60,
+        enabled: issueQuery.data !== undefined,
+    })
+
     return {
-        issueQuery
+        issueQuery,
+        commentsQuery
     }
 }
