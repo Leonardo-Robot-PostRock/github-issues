cc1416e0f1b8f76f05fc6ee19bdda974094cc7e4
Leonardo <leonardo38361@gmail.com>
2026-01-07 10:50:10 -0300

    feat: update LoadingSpinner component

--- src/shared/components/LoadingSpinner.tsx
+++ src/shared/components/LoadingSpinner.tsx
@@ -1,11 +1,15 @@
 import { FiRefreshCcw } from 'react-icons/fi'
 
-export const LoadingSpinner = () => {
+interface Props {
+    text?: string
+}
+
+export const LoadingSpinner = ({ text }: Props) => {
     return (
         <div className="flex justify-center items-center h-52">
             <div className="text-center">
                 <FiRefreshCcw className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 mx-auto mb-4" size={40} />
-                <p className="text-gray-600 font-medium">Cargando etiquetas...</p>
+                <p className="text-gray-600 font-medium">{text ?? "Loading..."}</p>
             </div>
         </div>
     )
