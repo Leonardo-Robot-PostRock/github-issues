6bc9e5cb7a48a98bc38cc9f380e84e762a0c3fdf
Leonardo <leonardo38361@gmail.com>
2026-01-07 10:50:02 -0300

    feat: update ListView component

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -13,7 +13,7 @@ export const ListView = () => {
     <div className="grid grid-cols-1 sm:grid-cols-3 mt-5">
       <div className="col-span-1 sm:col-span-2">
         {
-          issuesQuery.isLoading ? <LoadingSpinner /> : <IssueList issues={issues} />
+          issuesQuery.isLoading ? <LoadingSpinner text='Cargando lista de issues...'/> : <IssueList issues={issues} />
         }
       </div>
 
