6b5acc16e2f1d05a6944cb03afb8fff812c1a644
Leonardo <leonardo38361@gmail.com>
2026-01-07 18:02:30 -0300

    feat: enhance IssueItem with prefetching functionality
    - Add useQueryClient hook to prefetch data on mouse enter
    - Prefetch issue details and comments when hovering over an issue
    - Improves user experience by pre-loading data before navigation
    - Adds imports for getIssue and getIssueComments actions

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -2,6 +2,8 @@ import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 import { GithubIssue, State } from '../interfaces/issues.interface';
 import { FC } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { getIssue, getIssueComments } from '../actions';
 
 interface Props {
   issue: GithubIssue
@@ -9,9 +11,27 @@ interface Props {
 
 export const IssueItem: FC<Props> = ({ issue }) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient()
+
+
+  const prefetchData = () => {
+    queryClient.prefetchQuery({
+      queryKey: ['issues', issue.number],
+      queryFn: () => getIssue(issue.number),
+      staleTime: 1000 * 60
+    })
+
+    queryClient.prefetchQuery({
+      queryKey: ['issues', issue.number, 'comments'],
+      queryFn: () => getIssueComments(issue.number),
+      staleTime: 1000 * 60
+    })
+  }
 
   return (
-    <div className="animate-fadeIn flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
+    <div
+      onMouseEnter={prefetchData}
+      className="animate-fadeIn flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
 
       {
         issue.state === State.Open ? (
