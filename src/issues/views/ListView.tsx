08e76823a170ef4942e9cfc1cb2a304f557d6d47
Leonardo <leonardo38361@gmail.com>
2026-01-09 11:50:03 -0300

    UX móvil: reordenar layout para mostrar LabelPicker arriba en pantallas pequeñas

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -7,13 +7,22 @@ import { State } from '../interfaces/issues.interface';
 
 export const ListView = () => {
   const [state, setState] = useState<State>(State.All);
-  const { issuesQuery } = useIssues();
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+
+  const { issuesQuery } = useIssues({
+    state: state,
+    selectedLabels: selectedLabels
+  });
 
   const issues = issuesQuery.data ?? [];
 
+  const onLabelSelected = (labels: string[]) => {
+    setSelectedLabels(labels);
+  }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 mt-5">
-      <div className="col-span-1 sm:col-span-2">
+      <div className="col-span-1 sm:col-span-2 order-2 sm:order-1">
         {
           issuesQuery.isLoading
             ? <LoadingSpinner text='Cargando lista de issues...' />
@@ -21,8 +30,11 @@ export const ListView = () => {
         }
       </div>
 
-      <div className="col-span-1 px-2">
-        <LabelPicker />
+      <div className="col-span-1 order-1 sm:order-2 px-2">
+        <LabelPicker
+          onLabelSelected={onLabelSelected}
+          selectedLabels={selectedLabels}
+        />
       </div>
     </div>
   );
