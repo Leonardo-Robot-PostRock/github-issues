42d811c8ff7133acff1d7a0dfa779903d4d6f8ac
Leonardo <leonardo38361@gmail.com>
2026-01-12 12:47:37 -0300

    feat(issues): support per_page parameter in getIssues

--- src/issues/actions/get-issues-action.ts
+++ src/issues/actions/get-issues-action.ts
@@ -2,15 +2,18 @@ import { githubApi } from "../../api/github.api"
 import { sleep } from "../../helpers"
 import { GithubIssue, State } from "../interfaces/issues.interface"
 
-export const getIssues = async (state: State, selectedLabels: string[]): Promise<GithubIssue[]> => {
+export const getIssues = async (state: State, selectedLabels: string[], page:number, perPage = 5): Promise<GithubIssue[]> => {
     await sleep(1500)
 
     const params = new URLSearchParams();
-    params.append('state', state)
-
+    
     if (selectedLabels.length > 0) {
         params.append('labels', selectedLabels.join(','))
     }
+    
+    params.append('page', `${page}`)
+    params.append('state', state)
+    params.append('per_page', `${perPage}`)
 
     const { data } = await githubApi.get<GithubIssue[]>('/issues', {
         params
